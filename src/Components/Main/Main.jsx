9663b59466b7841daabf55a200ab3a909b4d13b7
MridulTailor <mtailor729@gmail.com>
2023-05-03 16:35:47 +0530

    first commit

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,111 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./main.css";
+import img from "../../Assets/img.jpg";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { HiOutlineClipboardCheck } from "react-icons/hi";
+import Aos from "aos";
+import "aos/dist/aos.css";
+const Data = [
+  {
+    id: 1,
+    imgSrc: img,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    grade: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is a living postcard, an Indonesian paradise that feels like a fantasy. Soak up the sun on a stretch of fine white sand, or commune with the tropical creatures as you dive along coral ridges or the colorful wreck of a WWII war ship. On shore, the lush jungle shelters stone temples and mischievous monkeys. The “artistic capital” of Ubud is the perfect place to see a cultural dance performance, take a batik or silver-smithing workshop, or invigorate your mind and body in a yoga class.",
+  },
+  {
+    id: 2,
+    imgSrc: img,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    grade: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is a living postcard, an Indonesian paradise that feels like a fantasy. Soak up the sun on a stretch of fine white sand, or commune with the tropical creatures as you dive along coral ridges or the colorful wreck of a WWII war ship. On shore, the lush jungle shelters stone temples and mischievous monkeys. The “artistic capital” of Ubud is the perfect place to see a cultural dance performance, take a batik or silver-smithing workshop, or invigorate your mind and body in a yoga class.",
+  },
+  {
+    id: 3,
+    imgSrc: img,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    grade: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is a living postcard, an Indonesian paradise that feels like a fantasy. Soak up the sun on a stretch of fine white sand, or commune with the tropical creatures as you dive along coral ridges or the colorful wreck of a WWII war ship. On shore, the lush jungle shelters stone temples and mischievous monkeys. The “artistic capital” of Ubud is the perfect place to see a cultural dance performance, take a batik or silver-smithing workshop, or invigorate your mind and body in a yoga class.",
+  },
+  {
+    id: 4,
+    imgSrc: img,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    grade: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is a living postcard, an Indonesian paradise that feels like a fantasy. Soak up the sun on a stretch of fine white sand, or commune with the tropical creatures as you dive along coral ridges or the colorful wreck of a WWII war ship. On shore, the lush jungle shelters stone temples and mischievous monkeys. The “artistic capital” of Ubud is the perfect place to see a cultural dance performance, take a batik or silver-smithing workshop, or invigorate your mind and body in a yoga class.",
+  },
+  {
+    id: 5,
+    imgSrc: img,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    grade: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is a living postcard, an Indonesian paradise that feels like a fantasy. Soak up the sun on a stretch of fine white sand, or commune with the tropical creatures as you dive along coral ridges or the colorful wreck of a WWII war ship. On shore, the lush jungle shelters stone temples and mischievous monkeys. The “artistic capital” of Ubud is the perfect place to see a cultural dance performance, take a batik or silver-smithing workshop, or invigorate your mind and body in a yoga class.",
+  },
+];
 const Main = () => {
-  return <div>Main</div>;
-};
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+  return (
+    <section className="main container section">
+      <div className="secTitle">
+        <h3 data-aos="fade-right" className="title">
+          Most visited destinations.
+        </h3>
+      </div>
+      <div className="secContent grid">
+        {Data.map(
+          ({ id, imgSrc, destTitle, location, grade, fees, description }) => {
+            return (
+              <div key={id} data-aos="fade-up" className="singleDestination">
+                <div className="imageDiv">
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
 
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <HiOutlineLocationMarker className="icon" />
+                    <span className="name">{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>
+                        {grade}
+                        <small>+1</small>
+                      </span>
+                    </div>
+                    <div className="price">{fees}</div>
+                  </div>
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+                  <button className="btn flex">
+                    DETAILS <HiOutlineClipboardCheck className="icon" />
+                  </button>
+                </div>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </section>
+  );
+};
 export default Main;
